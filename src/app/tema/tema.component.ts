d2bbd105e85a11b10c6d593ea999a5e4658f931b
Nando-PC <nandowarrior@gmail.com>
2022-06-29 21:29:35 -0300

    Complementação de acessibilidade do administrador

--- src/app/tema/tema.component.ts
+++ src/app/tema/tema.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment.prod';
 import { Tema } from '../model/Tema';
+import { AlertasService } from '../service/alertas.service';
 import { TemaService } from '../service/tema.service';
 
 @Component({
@@ -16,7 +17,8 @@ export class TemaComponent implements OnInit {
 
   constructor(
     private router: Router,
-    private temaService: TemaService
+    private temaService: TemaService,
+    private alertas: AlertasService
 
   ) { }
 
@@ -27,6 +29,12 @@ export class TemaComponent implements OnInit {
       this.router.navigate(['/entrar'])
 
     }
+
+    if(environment.tipo != 'adm'){
+      this.alertas.showAlertInfo('Você precisa ser administrador para acessar esta rota')
+      this.router.navigate(['/inicio'])
+    }
+
     this.findAllTemas()
   }
 
